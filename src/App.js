9642c0b17e60988e112034b269dccce3ed63efaa
Mohammad Ali Sakib <mohammad.ali.sakib.pk@gmail.com>
2021-10-29 09:37:59 +0600

    initial file setup are ready

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+function App() {
+  return (
+    <div>
+       <h1 className='text-center text-purple-500 font-bold'>Hello sakib</h1>
+    </div>
+  );
+}
+
+export default App;
